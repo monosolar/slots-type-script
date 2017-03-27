ee01000e97153d0916fc34ddf23f85965b86a61d
monosolar <monosolar.in@gmail.com>
2017-03-27 04:13:04 +0300

    without music

--- TSClasses.ts
+++ TSClasses.ts
@@ -11,23 +11,23 @@
 
  ** used TweenMax, but no
  ** hard to reach class methods inside nested functs.
- *
+ * layouting
  */
 
 
 module Slots {
 
     interface ReelItem {
-        row:    [];
-        init:     number;
+        row:[];
+        init:number;
     }
 
     class Mocks {
-        public static reelMock = '{ "sequences":[     {"row":[5,6,8,7,5,4], "init":3},' +
-                                               '{"row":[11,6,5,2,10,6,3,1], "init":2},' +
-                                               '{"row":[8,9,12,11,7,8,4,3], "init":1},' +
-                                               '{"row":[9,10,12,6,2,8,9,4,7], "init":4},' +
-                                               '{"row":[3,0,8,11,6,13,12,6,7,10], "init":6} ] }';
+    public static reelMock = '{ "sequences":[        {"row":[5,6,8,7,5,4], "init":3},' +
+                                                    '{"row":[11,6,5,2,10,6,3,1], "init":2},' +
+                                                    '{"row":[8,9,12,11,7,8,4,3], "init":1},' +
+                                                    '{"row":[9,10,12,6,2,8,9,4,7], "init":4},' +
+                                                    '{"row":[3,6,8,11,6,13,12,6,7,10], "init":6} ] }';
     }
 
     class Collections {
@@ -35,79 +35,46 @@ module Slots {
     }
 
     class Utils {
-        public static ticker: PIXI.ticker.Ticker;
+        public static ticker:PIXI.ticker.Ticker;
     }
 
-    export class Basics {
+    export class Application {
 
         private app:PIXI.Application;
 
-        private reelsSequenceComponent: ReelsSequenceComponent;
+        private reelsSequenceComponent:SpinButton;
+
+        private spinButton:PIXI.Sprite;
 
         constructor() {
 
             let appWidth = 1130;
-            let appHeight = 660;
-
-            let curtainHeight = 614;
-
-            let appMiddle = curtainHeight / 2;
+            let appHeight = 700;
 
             this.app = new PIXI.Application(appWidth, appHeight, {backgroundColor: 0x0b991b});
             document.body.appendChild(this.app.view);
+
             Utils.ticker = this.app.ticker;
-
             this.fillSignTexturesArray();
-
-
-
-
 
             let curtainContainer = new PIXI.Container();
             let curtainImgSprite = PIXI.Sprite.fromImage("./assets/img/slotOverlay.png");
             curtainContainer.addChild(curtainImgSprite);
 
+            this.spinButton = new SpinButton();
+            this.spinButton.position.set(appWidth / 2, appHeight - 70);
+            this.spinButton.anchor.set(0.5, 0.5);
+            this.spinButton.on('pointerdown', this.onSpinButtonClick, this);
 
-            let rectGraphic = new PIXI.Graphics();
-            rectGraphic.lineStyle(2, 0x0000FF, 1);
-            rectGraphic.beginFill(0xFF700B, 0.5);
-            rectGraphic.drawRoundedRect(0, 0, 1050, 560, 20, 20);
-
-            let rectSprite = new PIXI.Sprite(rectGraphic.generateCanvasTexture());
-            rectSprite.x = appWidth / 2;
-            rectSprite.y = curtainHeight / 2;
-
-            rectSprite.anchor.set(0.5, 0.5);
-
-            //curtainContainer.addChild(rectSprite );
-
-            let middleLineGraphic = new PIXI.Graphics();
-            middleLineGraphic.lineStyle(2, 0xFF0000, 1);
-            middleLineGraphic.moveTo(0, 0);
-            middleLineGraphic.lineTo(appWidth, 0);
-
-            let middleLineSprite = new PIXI.Sprite(middleLineGraphic.generateCanvasTexture());
-            middleLineSprite.y = appMiddle;
-
-            curtainContainer.addChild(middleLineSprite);
-
-            let spinButton = new SpinButton();
-            spinButton.position.set(appWidth / 2, appHeight - spinButton.height);
-            spinButton.anchor.set(0.5, 0.5);
-
-            curtainContainer.addChild(spinButton);
+            curtainContainer.addChild(this.spinButton);
 
             this.reelsSequenceComponent = new ReelsSequenceComponent();
-
-            this.reelsSequenceComponent.position.set(100,100);
-
-
+            this.reelsSequenceComponent.x = 32;
+            this.reelsSequenceComponent.y = 33;
 
             this.app.stage.addChild(this.reelsSequenceComponent);
 
-
             this.app.stage.addChild(curtainContainer);
-
         }
 
         private fillSignTexturesArray():void {
@@ -118,8 +85,14 @@ module Slots {
             }
         }
 
-    }
+        private onSpinButtonClick = () => {
+            this.reelsSequenceComponent.addListener('ON_FINISHED', ()=> {
+                this.spinButton.enabled = true;
+            });
 
+            this.reelsSequenceComponent.start();
+        }
+    }
 
     class SpinButton extends PIXI.Sprite {
 
@@ -146,7 +119,14 @@ module Slots {
                 .on('pointerupoutside', this.onButtonUp)
                 .on('pointerover', this.onButtonOver)
                 .on('pointerout', this.onButtonOut);
+        }
 
+        public set enabled(value:Boolean):void {
+            this.interactive = value;
+            this.buttonMode = value;
+
+            if (value)
+                this.texture = this.textureButton;
         }
 
         private onButtonDown():void {
@@ -158,11 +138,9 @@ module Slots {
         private onButtonUp():void {
             this.isdown = false;
             if (this.isOver) {
-                this.texture = this.textureButtonHover;
+                this.texture = this.textureButtonDisable;
             }
-            else {
-                this.texture = this.textureButton;
-            }
+            this.enabled = false;
         }
 
         private onButtonOver():void {
@@ -180,60 +158,76 @@ module Slots {
             }
             this.texture = this.textureButton;
         }
-
     }
 
     class ReelsSequenceComponent extends PIXI.Sprite {
 
-        private fixedWidth = 1000;
-        private fixedHeight = 600;
+        public static ON_FINISHED = 'ON_FINISHED';
 
-        private reelsArray: ReelComponent[] = [];
+        private fixedWidth = 1050;
+        private fixedHeight = 570;
+
+        private reelsArray:ReelComponent[] = [];
 
         constructor() {
+
             super();
 
-            // background layer
-            // ---------------
-            // reels
-            // ---------------
-            //
+            let maskedSprite = new PIXI.Sprite();
 
             let background = new PIXI.extras
                 .TilingSprite(PIXI.Texture.fromImage("./assets/img/winningFrameBackground.jpg"));
             background.width = this.fixedWidth;
             background.height = this.fixedHeight;
-            this.addChild(background)
+            maskedSprite.addChild(background)
 
-            let reelsParsedArray: ReelItem[] = JSON.parse(Mocks.reelMock).sequences;
+            let reelsParsedArray:ReelItem[] = JSON.parse(Mocks.reelMock).sequences;
 
-            this.reelsArray = [ new ReelComponent,
-                                new ReelComponent,
-                                new ReelComponent,
-                                new ReelComponent,
-                                new ReelComponent ];
+            this.reelsArray = [new ReelComponent,
+                new ReelComponent,
+                new ReelComponent,
+                new ReelComponent,
+                new ReelComponent];
 
             this.reelsArray.forEach((reelComponent, index) => {
                 reelComponent.signsIDsArray = reelsParsedArray[index].row;
                 reelComponent.createMainColumnSprite();
-                //reelComponent.setReelToId(reelsParsedArray[index].init);
-                reelComponent.setReelToId(0);
-                reelComponent.x = index * (this.fixedWidth/this.reelsArray.length);
+                reelComponent.setReelToId(reelsParsedArray[index].init);
+                reelComponent.x = index * (this.fixedWidth / this.reelsArray.length);
                 reelComponent.y = this.fixedHeight / 2;
-                reelComponent.pivot.x = -this.fixedWidth/this.reelsArray.length*0.5;
+                reelComponent.pivot.x = -this.fixedWidth / this.reelsArray.length * 0.5;
 
-                this.addChild(reelComponent)
+                maskedSprite.addChild(reelComponent)
             });
 
+            let rectGraphic = new PIXI.Graphics();
+            rectGraphic.lineStyle(2, 0x0000FF, 1);
+            rectGraphic.beginFill(0xFF700B, 0.5);
+            rectGraphic.drawRoundedRect(0, 0, this.fixedWidth, this.fixedHeight, 20, 20);
 
+            this.addChild(maskedSprite);
+            this.addChild(rectGraphic);
+
+            maskedSprite.mask = rectGraphic;
+        }
+
+        public start():void {
+            this.reelsArray.forEach((reelComponent, index) => {
+                reelComponent.startReel();
+                setTimeout(reelComponent.stopReel, 1000 + (index * 400), reelComponent);
+            });
+            this.reelsArray[this.reelsArray.length - 1].addListener('STOPPED', ()=> {
+                this.emit('ON_FINISHED', this);
+            })
 
         }
     }
 
     class ReelComponent extends PIXI.Sprite {
 
-        public signsIDsArray: number[];
-        public startSignID: number;
+        public static STOPPED = 'STOPPED';
+
+        public signsIDsArray:number[];
 
         private signHeight:number = 178;
         private signVPadding:number = -20;
@@ -247,21 +241,12 @@ module Slots {
 
         constructor() {
             super();
-
-            //this.signsIDsArray = signsIDsArray;
-
-            /*this.createMainColumnSprite();
-            this.setReelToId(0);
-
-            this.startReel();
-
-            setTimeout(this.stopReel, 1500, this)*/
         }
 
         public startReel():void {
 
             let speed = 0.3;
-            let maxSpeed = 15;
+            let maxSpeed = 20;
 
             this.animationFunction = function () {
 
@@ -287,47 +272,8 @@ module Slots {
             context.setReelToId(randomSign);
 
             context.mainColumnSpriteBlurAmount = 0;
-        }
 
-        private init():void {
-
-
-
-
-            // ==================================================
-
-            /*
-             var rotateCD = new TweenMax.to(this.reelSprite, 2, {y: -this.signStep * 2,
-             ease:Linear.easeNone,repeat:-1,paused:true}).timeScale(0);
-
-             play.onclick = function(){
-             rotateCD.play();
-             TweenLite.to(rotateCD,2,{timeScale:1});
-             };
-
-             pause.onclick = function(){
-             TweenLite.to(rotateCD,2,{timeScale:0,onComplete:function(){ this.pause() }})
-             };*/
-
-
-            /*var animation = new TweenMax.to(reelSprite, 2, {y: -this.signStep * 2,
-             repeatDelay:0.5, ease:Linear.easeNone, onComplete:function() {
-
-             reelSprite.y = -referencedSignStep * 6;
-
-             TweenMax.to(reelSprite, 2, {y: -referencedSignStep * 2, repeat: -1, ease:Linear.easeNone});
-
-             }});*/
-
-            //TweenMax.fromTo(animation,2,{timeScale:0},{timeScale:1})
-
-            /* var animation = new TimelineLite()
-             animation
-             .to(reelSprite, 2, {y:-this.signStep * 2, ease:Linear.easeNone})
-             .to(reelSprite, 3, {x:500, ease:Linear.easeNone});*/
-
-
-
+            context.emit('STOPPED');
         }
 
         public createMainColumnSprite():void {
@@ -349,8 +295,8 @@ module Slots {
 
         private set mainColumnSpriteBlurAmount(value:number):void {
             if (!this.reelSprite.filters) {
+                this.reelSprite.filters = [new PIXI.filters.BlurYFilter()];
             }
-            this.reelSprite.filters = [new PIXI.filters.BlurYFilter()];
 
             this.reelSprite.filters[0].blur = value;
         }
@@ -381,25 +327,13 @@ module Slots {
                 currentSignSprite = new PIXI.Sprite(Collections.signsTexturesArray[item - 1]);
                 currentSignSprite.anchor.set(0.5, 0.5)
                 currentSignSprite.y = this.signStep * index;
-
-                if (index == 4) {
-                    let enter = new PIXI.Graphics();
-                    enter.lineStyle(2, 0x0000FF, 1);
-                    enter.beginFill(0xF0B7F0, 1);
-                    enter.drawEllipse(0, 0, 10, 10);
-
-                    currentSignSprite.addChild(enter);
-                }
-
                 signsColumnSprite.addChild(currentSignSprite);
             });
 
             return signsColumnSprite;
         }
-
     }
-
 }
 
-new Slots.Basics();
+new Slots.Application();
 
